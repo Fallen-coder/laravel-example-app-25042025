2056e1db14cdd8af245f372254547dc968cb1c78
Your Name <you@example.com>
2025-04-25 08:29:19 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\App\Http\CommentController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,3 +19,14 @@ Route::get('/', function () {
 Route::get('/test', function (){
     return view('test', ['variable' => "my variable text value"]);
 });
+
+Route::get('/comment',[CommentController::class, 'index'] );
+
+
+
+
+
+
+
+
+
